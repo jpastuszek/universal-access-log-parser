b36d9a08508ebfff26a02fc783e30a3e4c149e91
Jakub Pastuszek <jpastuszek@gmail.com>
2011-10-03 18:34:35 +0200

    using - in main lib file

--- spec/common_parsers_spec.rb
+++ spec/common_parsers_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-require 'universal_access_log_parser'
+require 'universal-access-log-parser'
 require 'time'
 
 describe UniversalAccessLogParser do
